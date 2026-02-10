77769563781941c8df49aea0a81fc8dc2d2e3a7b
Johnny Brunton <162181450+JohnnyBrunton1@users.noreply.github.com>
2026-02-10 22:12:48 +0000

    update

---
+++ pydemo/in_class_if.py
@@ -0,0 +1,6 @@
+enter_int = int(input("Enter an integer:"))
+
+if enter_int % 5 == 0:
+    print("HiFive")
+if enter_int % 2 == 0:
+    print("HiEven")
---
+++ pydemo/in_class_number_demo.py
@@ -0,0 +1,10 @@
+number = int(input("Enter a number: "))
+
+if number % 15 == 0:
+    print("fizzbuzz")
+elif number % 5 == 0:
+    print("buzz")
+elif number % 3 == 0:
+    print("fizz")
+else:
+    print(number)
